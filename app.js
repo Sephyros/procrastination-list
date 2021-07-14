a7fb280a67bad5620b9bc706a3ad727c82a3b7d5
Sephyros <leo037br@hotmail.com>
2021-07-13 23:09:34 -0300

    Changed some colors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const hideUnwantedTodos = (todoArray, searchedValue) => {
 const addTodoToContainer = (todo) => {
   if (todo.length) {
     todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
+    <li class="bg-dark border-warning list-group-item d-flex justify-content-between align-items-center">
       <span>${todo}</span>
       <i class="text-danger far fa-trash-alt delete"></i>
     </li>`;
