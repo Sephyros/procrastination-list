41749df1fb824eec44f590fb0aa1eb29648558a2
Sephyros <leo037br@hotmail.com>
2021-07-13 22:45:06 -0300

    Some refactorings...

--- app.js
+++ app.js
@@ -2,14 +2,42 @@ const formAddTodo = document.querySelector(".form-add-todo");
 const todosContainer = document.querySelector(".todos-container");
 const formSearch = document.querySelector(".form-search");
 
+const showMatchingTodos = (todoArray, searchedValue) => {
+  const matchingTodos = todoArray.filter(
+    (todo) => !todo.textContent.toLowerCase().includes(searchedValue)
+  );
+  matchingTodos.forEach((todo) => {
+    todo.classList.remove("d-flex");
+    todo.classList.add("hidden");
+  });
+};
+
+const hideUnwantedTodos = (todoArray, searchedValue) => {
+  todoArray
+    .filter((todo) => todo.textContent.toLowerCase().includes(searchedValue))
+    .forEach((todo) => {
+      todo.classList.add("d-flex");
+      todo.classList.remove("hidden");
+    });
+};
+
+const addTodoToContainer = (todo) => {
+  if (todo.length) {
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span>${todo}</span>
+      <i class="text-danger far fa-trash-alt delete"></i>
+    </li>`;
+  }
+};
+
 formAddTodo.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const inputValue = event.target.add.value.trim();
-  todosContainer.innerHTML += `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${inputValue}</span>
-      <i class="far fa-trash-alt delete"></i>
-    </li>`;
+
+  addTodoToContainer(inputValue);
+
   event.target.reset();
 });
 
@@ -22,16 +50,8 @@ todosContainer.addEventListener("click", (event) => {
 
 formSearch.addEventListener("input", (event) => {
   const inputValue = event.target.value.toLowerCase();
-  Array.from(todosContainer.children)
-    .filter((todo) => !todo.textContent.toLowerCase().includes(inputValue))
-    .forEach((todo) => {
-      todo.classList.remove("d-flex");
-      todo.classList.add("hidden");
-    });
-  Array.from(todosContainer.children)
-    .filter((todo) => todo.textContent.toLowerCase().includes(inputValue))
-    .forEach((todo) => {
-      todo.classList.add("d-flex");
-      todo.classList.remove("hidden");
-    });
+  const todosArray = Array.from(todosContainer.children);
+
+  showMatchingTodos(todosArray, inputValue);
+  hideUnwantedTodos(todosArray, inputValue);
 });
